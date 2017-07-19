d5a017e5d1644c23f36e00b0901dbe214ba682a3
NateScarlet <NateScarlet@Gmail.com>
2017-07-19 17:11:36 +0800

    comp 1.3.4
    Former-commit-id: bfdc475cfc3240c6f42fdda5009a09844f1e202b

--- py/wlf/comp.py
+++ py/wlf/comp.py
@@ -16,7 +16,7 @@ from subprocess import PIPE, Popen
 import nuke
 import nukescripts
 
-__version__ = '1.3.3'
+__version__ = '1.3.4'
 
 OS_ENCODING = locale.getdefaultlocale()[1]
 SCRIPT_CODEC = 'UTF-8'
@@ -479,7 +479,7 @@ class Comp(object):
             '|| [value _ZDefocus.disable]} {return True} else {return False}]}}'
         )
         if 'motion' in nuke.layers(n):
-            n = nuke.nodes.VectorBlur(inputs=[n], disable=True)
+            n = nuke.nodes.VectorBlur2(inputs=[n], disable=True)
         n = nuke.nodes.Crop(
             inputs=[n],
             box='0 0 root.width root.height')
