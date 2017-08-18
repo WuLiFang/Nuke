babba9bd02b7c02ebd556a271a1db632812b7d5d
NateScarlet <NateScarlet@Gmail.com>
2017-08-18 14:19:35 +0800

    Positionkeyer cpp
    Former-commit-id: 62c6e40a7aefbcbd4981738a148fec578e1aa2f8

--- plugins/Keyer/PositionKeyer.cpp
+++ plugins/Keyer/PositionKeyer.cpp
@@ -1,5 +1,5 @@
 // PositionKeyerKernel
-// VERSION: 0.31
+// VERSION: 0.4.1
 kernel PositionKeyer : ImageComputationKernel<ePixelWise>
 {
   Image<eRead, eAccessPoint, eEdgeClamped> position;
@@ -18,22 +18,27 @@ local:
   float4 p0;
   float4 p1;
 
-  float4 rotate3(float4 pos, float ry)
+  float4 rotate3(float4 point, float3 rotate)
   {
-    float4 ret;
-    ret[0] = cos(ry) * pos[2] - sin(ry) * pos[0];
-    ret[1] = pos[1];
-    ret[2] = cos(ry) * pos[0] + sin(ry) * pos[2];
-    return ret;
+    float sx = sin(rotate.x);
+    float cx = cos(rotate.x);
+    float sy = sin(rotate.y);
+    float cy = cos(rotate.y);
+    float sz = sin(rotate.z);
+    float cz = cos(rotate.z);
+    float xy, xz, yx, yz, zx, zy;
+    xy = cx * point.y - sx * point.z;
+    xz = sx * point.y + cx * point.z;
+    yz = sy * point.x - cy * xz;
+    yx = cy * point.x + sy * xz;
+    zx = cz * yx - sz * xy;
+    zy = sz * yx + cz * xy;
+    point.x = zx;
+    point.y = zy;
+    point.z = yz;
+    return point;
   }
 
-  float2 rotate2(float a, float b, float angle)
-  {
-    float2 ret;
-    ret[0] = cos(angle) * b - sin(angle) * a;
-    ret[1] = cos(angle) * a + sin(angle) * b;
-    return ret;
-  }
   float linear(float x, float p0, float p1)
   {
     return (x - p0) / (p1 - p0);
@@ -41,21 +46,18 @@ local:
 
   void init()
   {
-    p0 = rotate3(p0_color, rotate_y);
-    p1 = rotate3(p1_color, rotate_y);
+    p0 = rotate3(p0_color, rotate);
+    p1 = rotate3(p1_color, rotate);
   }
 
   void process()
   {
-    float4 pos = rotate3(position(), rotate_y);
-    const float x = pos[0];
-    const float y = pos[1];
-    const float z = pos[2];
+    float4 pos = rotate3(position(), rotate);
 
     float4 result;
-    result[0] = linear(x, p0[0], p1[0]);
-    result[1] = linear(y, p0[1], p1[1]);
-    result[2] = linear(z, p0[2], p1[2]);
+    result[0] = linear(pos[0], p0[0], p1[0]);
+    result[1] = linear(pos[1], p0[1], p1[1]);
+    result[2] = linear(pos[2], p0[2], p1[2]);
     result = clamp(result, float4(0.0f), float4(1.0f));
 
     result[0] = invert_x ? (1 - result[0]) : result[0];
