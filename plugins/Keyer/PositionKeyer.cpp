87f06a4534c496a5dfb4fabfe18c6c37fdd4990c
NateScarlet <NateScarlet@Gmail.com>
2017-10-09 18:51:40 +0800

    PositionKeyer 1.4.0 # Add new mode
    Former-commit-id: 0fde56dc449c42a8017dd9fdeafea9fa30cf4291

--- plugins/Keyer/PositionKeyer.cpp
+++ plugins/Keyer/PositionKeyer.cpp
@@ -1,5 +1,5 @@
 // PositionKeyerKernel
-// VERSION: 0.4.4
+// VERSION: 0.5.0
 kernel PositionKeyer : ImageComputationKernel<ePixelWise>
 {
   Image<eRead, eAccessPoint, eEdgeClamped> position;
@@ -12,15 +12,17 @@ param:
   bool enable_x;
   bool enable_y;
   bool enable_z;
+  int mode;
   float3 rotate;
+  float3 scale;
   float4 p0_color;
   float4 p1_color;
 
 local:
-  float4 p0;
-  float4 p1;
+  float3 p0;
+  float3 p1;
 
-  float4 rotate3(float4 point, float3 rotate)
+  float3 rotate3(float3 point, float3 rotate)
   {
     float sx = sin(rotate.x);
     float cx = cos(rotate.x);
@@ -40,15 +42,22 @@ local:
     point.z = yz;
     return point;
   }
-
+  float3 rotate3(float4 point, float3 rotate)
+  {
+    return rotate3(to_float3(point), rotate);
+  }
   float linear(float x, float p0, float p1)
   {
-    if (p1 == p0){
+    if (p1 == p0)
+    {
       return p0;
     }
     return (x - p0) / (p1 - p0);
   }
-
+  float3 to_float3(float4 input)
+  {
+    return float3(input.x, input.y, input.z);
+  }
   void init()
   {
     p0 = rotate3(p0_color, rotate);
@@ -57,14 +66,29 @@ local:
 
   void process()
   {
-    float4 pos = rotate3(position(), rotate);
-
+    float3 pos = rotate3(position(), rotate);
     float4 result;
-    result[0] = linear(pos[0], p0[0], p1[0]);
-    result[1] = linear(pos[1], p0[1], p1[1]);
-    result[2] = linear(pos[2], p0[2], p1[2]);
-    result = clamp(result, float4(0.0f), float4(1.0f));
 
+    if (mode == 0)
+    {
+      result[0] = linear(pos.x, p0.x, p1.x);
+      result[1] = linear(pos.y, p0.y, p1.y);
+      result[2] = linear(pos.z, p0.z, p1.z);
+    }
+    else if (mode == 1)
+    {
+      result[0] = 1 - fabs(pos.x - p1.x) / fabs(p1.x - p0.x) / scale.x;
+      result[1] = 1 - fabs(pos.y - p1.y) / fabs(p1.y - p0.y) / scale.y;
+      result[2] = 1 - fabs(pos.z - p1.z) / fabs(p1.z - p0.z) / scale.z;
+    }
+    else if (mode == 2)
+    {
+      float ret = 1 - length((pos - p1) / scale) / (length(p1 - p0));
+      result[0] = ret;
+      result[1] = ret;
+      result[2] = ret;
+    }
+    result = clamp(result, float4(0.0f), float4(1.0f));
     result[0] = invert_x ? (1 - result[0]) : result[0];
     result[1] = invert_y ? (1 - result[1]) : result[1];
     result[2] = invert_z ? (1 - result[2]) : result[2];
