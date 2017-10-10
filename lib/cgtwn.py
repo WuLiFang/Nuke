b76547c95ad8f41d45cb812bdf8867ffbb959cad
NateScarlet <NateScarlet@Gmail.com>
2017-10-10 13:32:51 +0800

    cgtwn 0.9.17 # Bugfix
    Former-commit-id: 7b0be5282d0595d57100873f712d4c649a45feae

--- lib/cgtwn.py
+++ lib/cgtwn.py
@@ -18,7 +18,7 @@ import wlf.config
 from asset import copy
 from node import Last
 
-__version__ = '0.9.16'
+__version__ = '0.9.17'
 
 LOGGER = logging.getLogger('com.wlf.cgtwn')
 
@@ -194,12 +194,14 @@ def on_close_callback():
         start_time = time.time()
         # Wait on close jpg rendering.
         while True:
-            mtime = os.path.getmtime(shot.image)
-            if mtime - start_time > -10:
-                break
-            elif time.time() - start_time > 10:
-                LOGGER.warning('单帧等待时间超时: %s', shot.image)
-                break
+            try:
+                mtime = os.path.getmtime(shot.image)
+                if mtime - start_time > -10:
+                    break
+                elif time.time() - start_time > 10:
+                    LOGGER.warning('单帧等待时间超时: %s', shot.image)
+            except OSError:
+                pass
             time.sleep(1)
         dst = shot.upload_image()
         if dst:
