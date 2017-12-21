9faa3d3f3025f20009ae77a569ddfdce2f06da3b
NateScarlet <NateScarlet@Gmail.com>
2017-12-21 15:55:34 +0800

    Add tests.

---
+++ lib/tests/test_init.py
@@ -0,0 +1,10 @@
+# -*- coding=UTF-8 -*-
+"""Nuke initiate test.  """
+from __future__ import absolute_import
+from unittest import TestCase
+
+
+class NukeTestCase(TestCase):
+
+    def test_init(self):
+        import init
