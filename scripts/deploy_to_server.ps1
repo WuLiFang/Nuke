d9139e8f8c74786f61f4a0b041e16ee087854ff2
NateScarlet <NateScarlet@Gmail.com>
2020-08-17 15:17:07 +0800

    chore: refactor deploy script

--- scripts/deploy_to_server.ps1
+++ scripts/deploy_to_server.ps1
@@ -6,11 +6,16 @@ git.exe -C $(git remote get-url server) checkout $(git rev-parse HEAD)
 if (!$?) {
     exit 1
 }
-Robocopy.exe lib\site-packages "$(git remote get-url server)\lib\site-packages" /MIR /R:0
+git.exe -C docs/build/html push server
 if (!$?) {
     exit 1
 }
 git.exe -C "$(git remote get-url server)/docs/build/html" checkout $(git rev-parse gh-pages)
+if (!$?) {
+    exit 1
+}
+
+Robocopy.exe lib\site-packages "$(git remote get-url server)\lib\site-packages" /MIR /R:0
 if (!$?) {
     exit 1
 }
