a40afc59cdbff2ee7a42868465c90c179f645dcf
NateScarlet <NateScarlet@Gmail.com>
2020-08-20 15:55:07 +0800

    chore: update deploy script

--- scripts/deploy_to_server.ps1
+++ scripts/deploy_to_server.ps1
@@ -1,21 +1,17 @@
-git.exe push server
-if (!$?) {
-    exit 1
-}
-git.exe -C $(git remote get-url server) checkout $(git rev-parse HEAD)
-if (!$?) {
-    exit 1
-}
-git.exe -C docs/build/html push server
-if (!$?) {
-    exit 1
-}
-git.exe -C "$(git remote get-url server)/docs/build/html" checkout $(git rev-parse gh-pages)
-if (!$?) {
-    exit 1
+$ErrorActionPreference = "Stop"
+
+function Invoke-NativeCommand {
+    "+ $args"
+    $command = $args[0]
+    $arguments = $args[1..($args.Length)]
+    & $command @arguments
+    if (!$?) {
+        Throw "exit code ${LastExitCode}: ${args}"
+    }
 }
 
-Robocopy.exe lib\site-packages "$(git remote get-url server)\lib\site-packages" /MIR /R:0
-if (!$?) {
-    exit 1
-}
+Invoke-NativeCommand git fetch
+Invoke-NativeCommand git push server HEAD origin/gh-pages
+Invoke-NativeCommand git -C $(git remote get-url server) checkout $(git rev-parse HEAD)
+Invoke-NativeCommand git -C "$(git remote get-url server)/docs/build/html" checkout $(git rev-parse origin/gh-pages)
+Invoke-NativeCommand robocopy lib\site-packages "$(git remote get-url server)\lib\site-packages" /MIR /R:0
