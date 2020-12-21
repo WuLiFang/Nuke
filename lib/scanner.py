e93c86ac45943871bc668741f11296ac350a60f6
NateScarlet <NateScarlet@Gmail.com>
2020-12-21 19:10:03 +0800

    feat: support glob in empty folder scanner

--- lib/scanner.py
+++ lib/scanner.py
@@ -7,6 +7,7 @@ import os
 import re
 import sys
 import webbrowser
+import glob
 
 from Qt import QtCompat, QtCore, QtWidgets
 from Qt.QtWidgets import QFileDialog
@@ -19,8 +20,8 @@ class Config(wlf.config.Config):
     """A disk config can be manipulated like a dict."""
 
     default = {
-        'path': 'E:/test/server/SNJYW/Render',
-        'shot_pattern': '',
+        'patterns': 'E:/example/*',
+        'regex_pattern': '',
     }
     path = os.path.expanduser('~/.wlf.scanner.json')
 
@@ -49,16 +50,18 @@ class MainWindow(QtWidgets.QMainWindow):
                 self._config[k] = v
 
             for edit, key in self.edits_key.items():
+                if isinstance(edit, QtWidgets.QTextEdit):
+                    edit.textChanged.connect(
+                        lambda e=edit, k=key: _set_config(k, e.toPlainText())
+                    )
                 if isinstance(edit, QtWidgets.QLineEdit):
                     edit.editingFinished.connect(
                         lambda e=edit, k=key: _set_config(k, e.text())
                     )
-                    edit.editingFinished.connect(self.update_ui)
                 elif isinstance(edit, QtWidgets.QCheckBox):
                     edit.stateChanged.connect(
                         lambda state, k=key: _set_config(k, state)
                     )
-                    edit.stateChanged.connect(self.update_ui)
                 elif isinstance(edit, QtWidgets.QComboBox):
                     edit.currentIndexChanged.connect(
                         lambda index, ex=edit, k=key: _set_config(
@@ -67,11 +70,13 @@ class MainWindow(QtWidgets.QMainWindow):
                         )
                     )
                 else:
-                    print(u'待处理的控件: {} {}'.format(type(edit), edit))
+                    print(u'待处理的控件: {} {}'.format(type(edit), edit).encode("utf-8"))
 
             for qt_edit, k in self.edits_key.items():
                 try:
                     if isinstance(qt_edit, QtWidgets.QLineEdit):
+                        qt_edit.setText(self._config[k])
+                    if isinstance(qt_edit, QtWidgets.QTextEdit):
                         qt_edit.setText(self._config[k])
                     elif isinstance(qt_edit, QtWidgets.QCheckBox):
                         qt_edit.setCheckState(
@@ -87,78 +92,84 @@ class MainWindow(QtWidgets.QMainWindow):
         self._ui = QtCompat.loadUi(os.path.join(__file__, '../scanner.ui'))
         self.setCentralWidget(self._ui)
         self.edits_key = {
-            self.lineEditPath: 'path',
-            self.lineEditShotPattern: 'shot_pattern'
+            self.textEditPatterns: 'patterns',
+            self.lineEditRegexPattern: 'regex_pattern'
         }
+
+        self.textEditPatterns.textChanged.connect(
+            self.reset_status_bar,
+        )
+        self.lineEditRegexPattern.editingFinished.connect(
+            self.reset_status_bar,
+        )
         self._config = Config()
         self.labelVersion.setText('v{}'.format(__version__))
-        self.setWindowTitle(u'空文件夹扫描')
         self.resize(500, 600)
 
         _icon()
         _actions()
         _edits()
-        self._shots = []
+        self._result = []
+        self.reset_status_bar()
 
-    def showEvent(self, event):
-        self.update_ui()
-        event.accept()
+    def reset_status_bar(self):
+        self.statusBar().showMessage(u"点击刷新按钮更新结果".encode('utf-8'))
 
     def __getattr__(self, name):
         return getattr(self._ui, name)
 
+    @property
+    def patterns(self):
+        """Current pattern to scan.  """
+        return self.textEditPatterns.toPlainText().split("\n")
+
+    @patterns.setter
+    def patterns(self, value):
+        """Current pattern to scan.  """
+        return self.textEditPatterns.setText("\n".join(value))
+
+    @property
+    def regex_pattern(self):
+        """re match pattern.  """
+        return self.lineEditRegexPattern.text()
+
+    def ask_path(self):
+        """Show a dialog ask user self._config['DIR'].  """
+
+        _dir = QFileDialog.getExistingDirectory(self, dir="/")
+        if _dir:
+            self.patterns += [_dir + "/*"]
+
+    def result(self):
+        """match result.  """
+        ret = set()
+
+        for pattern in self.patterns:
+            self.statusBar().showMessage(pattern)
+            for i in glob.glob(pattern):
+                if not os.path.isdir(i):
+                    continue
+                if not re.match(self.regex_pattern, i):
+                    continue
+                if os.listdir(i):
+                    continue
+                ret.add(i)
+        self.statusBar().showMessage(u"扫描完毕".encode('utf-8'))
+
+        return sorted(ret)
+
     def update_ui(self):
         """Update dialog UI content.  """
 
-        start = len(self.path)
-        self._shots = list(i[start:] for i in self.shots())
-        self.listView.setModel(QtCore.QStringListModel(self._shots))
-
-    def shots(self):
-        """empty shots contained in path.  """
-        ret = []
-        for dirpath, dirnames, filenames in os.walk(self.path):
-            QtWidgets.QApplication.processEvents()
-            if re.match(self.shot_pattern, os.path.basename(dirpath)):
-                if not filenames and not dirnames:
-                    ret.append(dirpath)
-                del dirnames[:]
-            self.statusBar().showMessage(dirpath)
-        self.statusBar().clearMessage()
-
-        return sorted(ret)
+        self._result = self.result()
+        self.listView.setModel(QtCore.QStringListModel(self._result))
 
     def generate_txt(self):
         """Generate txt."""
         path = os.path.expanduser('~/wlf.scanner.txt')
         with open(path, 'w') as f:
-            f.write('\n'.join(self._shots))
+            f.write('\n'.join(self._result))
         webbrowser.open(path)
-
-    def ask_path(self):
-        """Show a dialog ask user self._config['DIR'].  """
-
-        default_dir = os.path.dirname(self.path)
-
-        _dir = QFileDialog.getExistingDirectory(self, dir=default_dir)
-        if _dir:
-            self.path = _dir
-
-    @property
-    def path(self):
-        """Current path to scan.  """
-        return self.lineEditPath.text()
-
-    @path.setter
-    def path(self, value):
-        """Current path to scan.  """
-        return self.lineEditPath.setText(value)
-
-    @property
-    def shot_pattern(self):
-        """Current shot re match pattern.  """
-        return self.lineEditShotPattern.text()
-
 
 def call_from_nuke():
     """Run this script standaloe.  """
