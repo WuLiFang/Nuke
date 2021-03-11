f40e55b26b7353a936e15a0333d2fddb37722b39
NateScarlet <NateScarlet@Gmail.com>
2021-03-11 17:22:38 +0800

    refactor: remove unused modules

--- lib/asset/__init__.py
+++ lib/asset/__init__.py
@@ -6,9 +6,5 @@ from __future__ import (absolute_import, division, print_function,
 
 from .autofix import fix_read
 from .comp import sent_to_dir
-from .footage import Footage
-from .frameranges import FrameRanges
-from .model import MissingFramesDict
-from .monitor import FootagesMonitor
 from .notify import warn_missing_frames, warn_mtime
 from .util import setup
