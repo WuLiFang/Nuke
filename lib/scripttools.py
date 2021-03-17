fe8b47be794361c2726a3b62cfe359060d6bc32f
NateScarlet <NateScarlet@Gmail.com>
2021-03-17 15:55:57 +0800

    chore: remove unused files

--- lib/scripttools.py
+++
@@ -1,8 +0,0 @@
-# -*- coding=UTF-8 -*-
-"""Tools for nuke comp scripts.  """
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-
-from nuketools import keep_modifield_status
-
-# TODO: remove this module.
