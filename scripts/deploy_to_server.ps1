ba21ede80f4d8b17c8dbc8fdf24be8ace05a8df1
NateScarlet <NateScarlet@Gmail.com>
2021-04-02 16:05:09 +0800

    chore: clean python cache on deploy

--- scripts/deploy_to_server.ps1
+++ scripts/deploy_to_server.ps1
@@ -13,5 +13,6 @@ function Invoke-NativeCommand {
 Invoke-NativeCommand git fetch -f origin gh-pages
 Invoke-NativeCommand git push -f server HEAD origin/gh-pages
 Invoke-NativeCommand git -C $(git remote get-url server) checkout $(git rev-parse HEAD)
+Invoke-NativeCommand git -C $(git remote get-url server) clean -fX *.pyc
 Invoke-NativeCommand git -C "$(git remote get-url server)/docs/build/html" checkout $(git rev-parse origin/gh-pages)
 Invoke-NativeCommand robocopy lib\site-packages "$(git remote get-url server)\lib\site-packages" /MIR /R:0
