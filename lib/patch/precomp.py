0e1e75e8f26af44706937ac96ac9bc7e712391ac
NateScarlet <NateScarlet@Gmail.com>
2021-07-19 12:10:09 +0800

    fix: error after precomp selected

--- lib/patch/precomp.py
+++ lib/patch/precomp.py
@@ -6,9 +6,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import logging
 
 import nuke
-import six
 
-import edit
 from pathlib2_unicode import PurePath
 import cast_unknown as cast
 
@@ -70,82 +68,15 @@ def _knob_changed(self, knob):
     {self.precompName: _on_precomp_name_changed,}.get(
         knob, lambda *_: None
     )(self, knob)
-    options = {name: k.value() for name, k in self.knobs().items()}
-    options = {
-        k: cast.text(v) if isinstance(v, six.binary_type) else v
-        for k, v in options.items()
-    }
-    PatchPrecompSelected.current_options = options  # type: ignore
-    assert PatchPrecompSelected.current_options
-
-
-class PatchPrecompSelected(BasePatch):
-    """Enhance precomp creation."""
-
-    target = "nukescripts.precomp_selected"
-    current_options = None
-    current_precomp_node = None
-
-    @classmethod
-    def func(cls, *args, **kwargs):
-        ret = cls.orig(*args, **kwargs)
-
-        if ret is None or not cls.current_options:
-            return ret
-
-        group = nuke.nodes.Group()
-        assert isinstance(group, nuke.Group)
-        with group:
-            nuke.scriptReadFile(cls.current_options["script"].encode("utf-8"))
-            write_nodes = nuke.allNodes(b"Write")
-            assert write_nodes
-            if len(write_nodes) != 1:
-                nuke.message("注意: 预合成中发现了多个输出节点, 可能导致渲染出错".encode("utf-8"))
-            precomp_name = cls.current_options["precomp_name"]
-            name = "_".join(
-                i for i in ("Write", precomp_name.upper().replace(" ", "_"), "1") if i
-            )
-            for n in write_nodes:
-                n.setName(name.encode("utf-8"))
-                # Disable exr hash check
-                # precomp node will change the value
-                # so we need a assert node.
-                assert_node = nuke.nodes.Assert()
-                _ = assert_node[b"expression"].setExpression(
-                    "[knob {}.checkHashOnRead 0]\n"
-                    "[return 1]".format(n.name()).encode("utf-8")
-                )
-                edit.insert_node(assert_node, n.input(0))
-            _ = nuke.tcl(
-                b"export_as_precomp", cls.current_options["script"].encode("utf-8")
-            )
-            if cls.current_precomp_node:
-                cls.current_precomp_node.reload()
-            else:
-                LOGGER.warning("Not found precomp node after `precomp_selected`")
-
-        nuke.delete(group)
-        cls.current_options = None
-        cls.current_precomp_node = None
-
-        return ret
-
-
-def _on_precomp_create():
-    PatchPrecompSelected.current_precomp_node = nuke.thisNode()  # type: ignore
 
 
 def enable():
     """Enable patch."""
 
     PatchPrecompDialog.enable()
-    PatchPrecompSelected.enable()
-    nuke.addOnCreate(_on_precomp_create, nodeClass=b"Precomp")
 
 
 def disable():
     """Disable patch."""
 
     PatchPrecompDialog.disable()
-    PatchPrecompSelected.disable()
-    nuke.removeOnCreate(_on_precomp_create, nodeClass=b"Precomp")
