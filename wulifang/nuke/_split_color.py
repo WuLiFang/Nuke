936182915e139e1da716b7c477009af018e83951
NateScarlet <NateScarlet@Gmail.com>
2023-09-18 18:47:00 +0800

    chore(nuke): split-color: improve UX

--- wulifang/nuke/_split_color.py
+++ wulifang/nuke/_split_color.py
@@ -16,6 +16,7 @@ if TYPE_CHECKING:
     from typing import Iterator, Text
 
 _MIN_ALPHA = 0.5
+_CHUNK_SIZE = 32
 
 
 def _iter_color(__node, grid=(100, 100)):
@@ -49,13 +50,21 @@ def _iter_color(__node, grid=(100, 100)):
 
 def _color_hex(color):
     # type: (tuple[float, float, float]) -> Text
-    return "#%02X%02X%02X" % (round(color[0] * 255), round(color[1] * 255), round(color[2] * 255))
+    return "#%02X%02X%02X" % (
+        round(color[0] * 255),
+        round(color[1] * 255),
+        round(color[2] * 255),
+    )
 
 
 def split(__node):
     # type: (nuke.Node) -> Iterator[nuke.Node]
 
-    c = Counter(_iter_color(__node))
+    c = Counter(i for i in _iter_color(__node) if i[3] >= _MIN_ALPHA)
+    if not c:
+        nuke.message(cast_str("没有 alpha 大于 %.0f 的像素" % (_MIN_ALPHA)))
+        return
+
     existing_color_hex = set()  # type: set[Text]
     for i in __node.dependent():
         if cast_text(i.name()).startswith("ColorKeyer"):
@@ -66,22 +75,30 @@ def split(__node):
                     v = (v, v, v)
                 existing_color_hex.add(_color_hex((v[0], v[1], v[2])))
 
-    for color, _ in c.most_common():
-        # skip transparent pixel
-        if color[3] < _MIN_ALPHA:
-            continue
+    created_count = 0
 
-        # skip existing color
-        color_hex = _color_hex(color[:3])
-        if color_hex in existing_color_hex:
-            continue
-        existing_color_hex.add(color_hex)
+    with Progress("创建节点") as p:
+        for index, v in enumerate(c.most_common()):
+            color = v[0]
+            color_hex = _color_hex(color[:3])
+            p.set_message(color_hex)
+            p.set_value(index / len(c))
 
-        n = create_node(
-            "ColorKeyer2",
-            inputs=(__node,),
-        )
-        knob_of(n, "color", nuke.AColor_Knob).setValue(
-            color,
-        )
-        yield n
+            # skip existing color
+            if color_hex in existing_color_hex:
+                continue
+            existing_color_hex.add(color_hex)
+
+            n = create_node(
+                "ColorKeyer2",
+                inputs=(__node,),
+            )
+            created_count += 1
+            knob_of(n, "color", nuke.AColor_Knob).setValue(
+                color,
+            )
+            yield n
+            if created_count % _CHUNK_SIZE == 0 and not nuke.ask(
+                cast_str("已经创建了 %d 个节点，继续？" % (created_count,))
+            ):
+                return
